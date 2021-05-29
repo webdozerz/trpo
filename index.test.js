56f34d84d4e667bff674c3d721e74acf32ea162a
iliyaandrushov <webdozerz@gmail.com>
2021-05-29 16:12:44 +0300

    [update] Tests

--- index.test.js
+++ index.test.js
@@ -1,21 +1,5 @@
 import { setupTest, createPage } from '@nuxt/test-utils';
 
-describe('browser', () => {
-  setupTest({
-    fixture: 'fixtures/basic',
-    browser: true,
-    browserOptions: {
-      type: 'chromium',
-    },
-  });
-
-  test('should render page', async () => {
-    const page = await createPage('/');
-    const body = await page.innerHTML('body');
-    expect(body).toContain('Works!');
-  }, 100000);
-});
-
 describe('browser type', () => {
   beforeEach(() => {
     jest.setTimeout(100000);
