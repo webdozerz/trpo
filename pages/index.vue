2fb24b47882ffc002411ec38d18342d0d90a567e
iliyaandrushov <webdozerz@gmail.com>
2021-05-29 18:14:39 +0300

    [update] Tests

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,14 @@
 <template>
-  <section>
-    <h2 class="title">
-      Disney Movies
-    </h2>
-    <ul class="movies">
-      <li
-        v-for="movie in disneyMovies"
-        :key="movie.title"
-      >
-        {{ movie.title }}
-      </li>
-    </ul>
-  </section>
+  <div class="main">
+    Main
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-
 export default {
-  computed: {
-    ...mapGetters({
-      byStudio: 'movies/byStudio',
-    }),
-    disneyMovies() {
-      return this.byStudio('Disney');
-    },
+  name: 'Index',
+  mounted() {
+    this.$router.push('/rooms');
   },
 };
 </script>
-
-<style>
-.title {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-</style>
