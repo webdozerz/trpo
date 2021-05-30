bd62958f65702d4d6040163273dc493933b328b2
iliyaandrushov <webdozerz@gmail.com>
2021-05-30 16:54:08 +0300

    [update] Workflows

--- dist/_nuxt/734a894.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{259:function(n,e,o){"use strict";o.r(e);var t={name:"Rooms"},c=o(52),component=Object(c.a)(t,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n  Rooms\n")])}),[],!1,null,"0540c3d9",null);e.default=component.exports}}]);
