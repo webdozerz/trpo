65cc2d51ddde886cc1ffa26b4ed7c01c859e8379
iliyaandrushov <webdozerz@gmail.com>
2021-06-06 12:27:04 +0300

    [update] Invoice

--- dist/_nuxt/static/1622970579/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622970579",layout:"default",error:a,state:{user:{rooms:[],consts:{}}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
---
+++ dist/_nuxt/static/1622971497/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622971497",layout:"default",error:a,state:{user:{rooms:[],consts:{}}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
